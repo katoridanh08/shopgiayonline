2759c24c26c0141f800634e744fef410df723b58
katoridanh08 <52204681+katoridanh08@users.noreply.github.com>
2019-09-24 19:32:18 +0700

    ad

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['prefix'=>'admin'],function(){
+    Route::group(['prefix'=>'danh-muc'],function(){
+        Route::get("them",['as'=>'themdanhmuc','uses'=>'CategoryController@getAddCate']);
+        Route::post("them",['as'=>'themdanhmuc','uses'=>'CategoryController@postAddCate']);
+        Route::get("danh-sach",['as'=>'listdanhmuc','uses'=>'CategoryController@getListCate']);
+
+
+    });
+
+});
